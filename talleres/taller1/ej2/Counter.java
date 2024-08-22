5185265f07d31dce5c03552947170e9a62c6090e
Jonas Glaubart <jglaubart@itba.edu.ar>
2024-08-22 20:37:48 -0300

    Primer commit

---
+++ talleres/taller1/ej2/Counter.java
@@ -0,0 +1,19 @@
+package taller1.ej2;
+
+public class Counter {
+    private int counter;
+
+    public Counter() {
+        counter = 0;
+    }
+
+    public void increase() {
+        this.counter++;
+    }
+    public void decrease(){
+        this.counter--;
+    }
+    public int getCounter(){
+        return this.counter;
+    }
+}
---
+++ talleres/taller1/ej3/MultipleCounter.java
@@ -0,0 +1,22 @@
+package taller1.ej3;
+
+import taller1.ej2.Counter;
+
+public class MultipleCounter extends Counter {
+    private int num;
+
+    public MultipleCounter(int times) {
+        this.num = times;
+    }
+
+    public void increase() {
+        for(int i=0; i<this.num; i++){
+            super.increase();   // con super llama al increase del padre
+        }
+    }
+    public void decrease(){
+        for(int i=0; i<this.num; i++){
+            super.decrease();
+        }
+    }
+}
